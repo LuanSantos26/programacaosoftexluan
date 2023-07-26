a8c3c1f9329842782b5550a5e153ebbb541bce88
LuanSantos26 <luansantos26@outlook.com>
2023-07-25 23:00:22 -0300

    exercício 01,02

---
+++ index.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')();
+
+//exercicio01
+function exercicio01(){
+    console.log("hello, word");
+}
+exercicio01();
+
+//exercicio2
+function exercicio2(){
+    let nome =prompt ("digite seu nome: ");
+    console.log ("olá "+nome);
+}
+exercicio2();
