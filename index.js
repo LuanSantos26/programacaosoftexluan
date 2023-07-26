ef3e202d1820ac0f731dc353b0f32d2091a94bcd
LuanSantos26 <luansantos26@outlook.com>
2023-07-26 19:27:40 -0300

    exercicio 3 ao 6

--- index.js
+++ index.js
@@ -16,7 +16,41 @@ exercicio2();
 //exercicio 3
 function exercicio3(){
     let numero =prompt("digite um numero para somar:");
+    numero=parseInt(numero);
     let numero2=prompt("digite outro numero para somar ");
-
+    numero2=parseInt(numero2)
+    let resultadosoma=(numero+numero2);
+    
+    console.log("A soma é " + resultadosoma)
 }
-console.log (exercicio3());
+ exercicio3();
+
+//exercicio 04
+function exercicio4(){
+    let idade=prompt("digite sua idade: ");
+    idade=parseInt(idade);
+    let maioridade = idade >= 18;
+    if (maioridade){
+        console.log("voce e maior de idade");
+    }else{
+        console.log("voce nao e maior de idade");
+    }
+}
+exercicio4();
+
+//exercicio05
+function exercicio5(){
+    let numero=prompt("digite um numero: ");
+    numero=parseInt(numero);
+    if (numero % 2 === 0){
+        console.log("o numero digitado e par");
+    }else{
+        console.log("o numero digitado e impar");
+    }
+}   
+    exercicio5();
+
+    //exercicio6
+    function exercicio6(){
+        
+    }
